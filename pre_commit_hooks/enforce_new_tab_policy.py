0fcc59ab67be4e0e29d7c4d4808aa6f5a50d5b99
ashley-hebler <ashleyhebler@gmail.com>
2019-06-03 15:02:02 -0500

    Try checking for rel

--- pre_commit_hooks/enforce_new_tab_policy.py
+++ pre_commit_hooks/enforce_new_tab_policy.py
@@ -9,24 +9,31 @@ def main(argv=None):
     parser.add_argument('--forbidden-attributes', type=lambda s: s.split(','), default=[],
                         help='Comma-separated list of forbidden attribute names')
     args = parser.parse_args(argv)
-    matches = list(iterate_forbidden_attributes(args.filenames, args.forbidden_attributes))
+    matches = list(iterate_attributes(args.filenames))
     return_error_code = 0
-    for filename, attr_name in matches:
-        print('Forbidden HTML attribute "{}" found in {}'.format(attr_name, filename))
+    for filename in matches:
+        print('target=_blank used with no rel found in {}'.format(filename))
         return_error_code = 1
     return return_error_code
 
-def iterate_forbidden_attributes(html_filenames, forbidden_attributes):
-    forbidden_attributes = set(forbidden_attributes)
+def iterate_attributes(html_filenames):
     for html_filename in html_filenames:
         with open(html_filename, 'rb') as html_file:
             for _, elem in iterparse(html_file, html=True, remove_comments=True):
                 print(elem.attrib)
+                new_window = False
+                includes_rel = False
                 for attribute_name in elem.attrib.keys():
-                    print(elem.attrib[attribute_name])
-                    if attribute_name in forbidden_attributes:
-                        yield html_filename, attribute_name  # sadly elem.sourceline is None :(
-
+                    val = elem.attrib[attribute_name]
+                    if attribute_name == 'target' and val = '_blank':
+                      new_window = True
+                    if attribute_name == 'rel' and val = 'noopener':
+                      includes_rel = True
+                # yield html_filename, new_window, includes_rel
+                if new_window and not includes_rel:
+                  yield html_filename
+                
+            
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
